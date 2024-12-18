02ca73df568a6041b2effaf294d667f6c204bbb9
Isaac Ramirez <1125167-isaacerd@users.noreply.gitlab.com>
2024-12-17 23:46:26 -0400

    fix

--- ios/FastImage/FFFastImageView.m
+++ ios/FastImage/FFFastImageView.m
@@ -73,12 +73,14 @@
 
 - (UIImage*) makeImage: (UIImage*)image withTint: (UIColor*)color {
     UIImage* newImage = [image imageWithRenderingMode: UIImageRenderingModeAlwaysTemplate];
-    UIGraphicsImageRenderer *renderer = [[UIGraphicsImageRenderer alloc] initWithSize:image.size];
-    newImage = [renderer imageWithActions:^(UIGraphicsImageRendererContext * _Nonnull rendererContext) {
-        [color setFill];
-    [newImage drawInRect: CGRectMake(0, 0, image.size.width, newImage.size.height)];
+    UIGraphicsImageRendererFormat *format = [UIGraphicsImageRendererFormat defaultFormat];
+    UIGraphicsImageRenderer *renderer = [[UIGraphicsImageRenderer alloc] initWithSize:image.size format:format];
+    UIImage *resultImage = [renderer imageWithActions:^(UIGraphicsImageRendererContext * _Nonnull rendererContext) {
+        CGRect rect = CGRectMake(0, 0, image.size.width, image.size.height);
+        [color set];
+        [newImage drawInRect:rect];
     }];
-    return newImage;
+    return resultImage;
 }
 
 - (void) setImage: (UIImage*)image {
